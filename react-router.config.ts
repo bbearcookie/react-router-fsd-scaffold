5c547193ccfc45b23e8ba9f0a376394a1a221de6
bbearcookie <aodem@naver.com>
2025-11-15 19:43:23 +0900

    refactor: react-router 관련 파일을 app 레이어로 이동

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@react-router/dev/config';
 
 export default {
-  appDirectory: 'src',
+  appDirectory: 'src/app',
   buildDirectory: 'dist',
   ssr: false,
 } satisfies Config;
--- src/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, route } from '@react-router/dev/routes';
-
-export default [
-  route('/', 'pages/Page.tsx'),
-  route('*?', 'pages/NotFoundPage.tsx'),
-] satisfies RouteConfig;
