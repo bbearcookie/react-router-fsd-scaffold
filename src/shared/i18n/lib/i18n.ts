b9b7e9b43751fc684bf7a3d0bda86989b0471e32
bbearcookie <aodem@naver.com>
2025-11-16 11:57:49 +0900

    refactor: I18n 언어 파일을 번들 애셋에서 제외하느 방식으로 수정

--- src/shared/i18n/lib/i18n.ts
+++ src/shared/i18n/lib/i18n.ts
@@ -1,23 +1,34 @@
 import i18n, { type InitOptions } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { fallbackLanguage, supportedLanguages, type SupportedLanguage } from '../config/languages';
-import ja from '../assets/ja.json';
-import ko from '../assets/ko.json';
-import en from '../assets/en.json';
+import resourcesToBackend from 'i18next-resources-to-backend';
+
+const isServer = typeof window === 'undefined';
 
 const initI18Next = async (i18next: typeof i18n, language?: SupportedLanguage) => {
   const options: InitOptions = {
     supportedLngs: supportedLanguages,
     fallbackLng: fallbackLanguage,
     lng: language || fallbackLanguage,
-    resources: {
-      ko: { translation: ko },
-      en: { translation: en },
-      ja: { translation: ja },
-    },
   };
 
-  await i18next.use(initReactI18next).init(options);
+  await i18next
+    .use(initReactI18next)
+    .use(
+      resourcesToBackend(async (language: string) => {
+        if (isServer) {
+          const fs = await import('node:fs/promises');
+          const path = await import('node:path');
+          const filePath = path.join(process.cwd(), 'public', 'locales', `${language}.json`);
+          const fileContent = await fs.readFile(filePath, 'utf-8');
+          return JSON.parse(fileContent);
+        } else {
+          const response = await fetch(`/locales/${language}.json`);
+          return response.json();
+        }
+      }),
+    )
+    .init(options);
 };
 
 export { i18n, initI18Next };
