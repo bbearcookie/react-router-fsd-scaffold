ef3cddac463cee364205741c9626b99f111dd22d
bbearcookie <aodem@naver.com>
2025-11-17 20:50:49 +0900

    chore: Vitest 설치

---
+++ src/shared/utils/calculator.test.ts
@@ -0,0 +1,7 @@
+import { expect, it } from 'vitest';
+import { sum } from './calculator';
+
+it('should return the sum of two numbers', () => {
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(2, 2)).toBe(4);
+});
