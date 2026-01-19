3372be3e245eb9e5092266036284177ba7a38e1c
bbearcookie <aodem@naver.com>
2026-01-19 20:31:22 +0900

    chore: 테스트 러너에 jest-dom 및 msw 적용

--- src/shared/testing-library/config/vitest.setup.ts
+++ src/shared/testing-library/config/vitest.setup.ts
@@ -1,6 +1,21 @@
 import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
+import { afterAll, afterEach, beforeAll, vi } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import { server } from '@/shared/msw/lib/msw.server';
+
+vi.spyOn(console, 'error').mockImplementation(() => undefined);
 
 afterEach(() => {
   cleanup();
 });
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+
+afterAll(() => server.close());
