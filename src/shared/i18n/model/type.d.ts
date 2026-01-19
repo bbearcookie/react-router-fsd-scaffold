0eae26d7b5a9f4a054526058417bd5cf0ac40a7d
bbearcookie <aodem@naver.com>
2026-01-19 22:30:33 +0900

    chore: i18n 키 타입 체크 추가

---
+++ src/shared/i18n/model/type.d.ts
@@ -0,0 +1,10 @@
+import ko from '@/../public/locales/ko.json';
+
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    defaultNS: 'translation';
+    resources: {
+      translation: typeof ko;
+    };
+  }
+}
